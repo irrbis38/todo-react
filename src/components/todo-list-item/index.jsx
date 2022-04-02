461da704817aee3060e72b8d994510593d638890
Vasily <v.ilyuhin@gmail.com>
2022-04-02 07:52:50 +0300

    feat: add state to todo-list-item

--- src/components/todo-list-item/index.jsx
+++ src/components/todo-list-item/index.jsx
@@ -3,23 +3,39 @@ import React from "react";
 import "./todo-list-item.css";
 
 class TodoListItem extends React.Component {
+  state = {
+    isDone: false,
+    important: false
+  };
+  onClickHandler = () => {
+    this.setState({isDone: true});
+  };
+  onMarkImportant = () => {
+    this.setState({important: true});
+  };
   render() {
-    const { label, important = false } = this.props;
+    const { label } = this.props;
+    const {isDone, important} = this.state;
 
     const style = {
       color: important ? "steelblue" : "black",
       fontWeight: important ? "bold" : "normal",
     };
 
+    let itemClass = "todo-list-item";
+    if(isDone){itemClass += " done"}
+    if(important){itemClass += " important"}
+
     return (
-      <span className="todo-list-item">
-        <span className="todo-list-item-label" style={style}>
+      <span className={itemClass}>
+        <span className="todo-list-item-label" style={style} onClick={this.onClickHandler}>
           {label}
         </span>
 
         <button
           type="button"
           className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}
         >
           <i className="fa fa-exclamation" />
         </button>
